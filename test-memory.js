a978a60cea46448cf542e2706db4a24a421b1974
rafanoobies-sys <rafael.durangegf2020@gmail.com>
2026-08-19 23:26:56 +0800

    Clean up project: remove old Supabase files, update package.json

--- test-memory.js
+++
@@ -1,6 +0,0 @@
-import { fixError } from './factory/fixer.js'
-
-const testError = new Error('Test error for GHOST fixer')
-
-const result = await fixError(testError)
-console.log('Fixer result:', result)
